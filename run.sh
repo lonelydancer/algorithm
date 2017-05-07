a947d04ff5bcf5b8c03a03fb23ffc4ae0fa4fe19
lonelydancer <penglonghu@gmail.com>
2017-05-07 21:59:33 +0800

    add cpp

---
+++ run.sh
@@ -0,0 +1,6 @@
+#echo "# algorithm" >> README.md
+#git init
+#git add README.md
+#git commit -m "first commit"
+git remote add origin https://github.com/lonelydancer/algorithm.git
+git push -u origin master
